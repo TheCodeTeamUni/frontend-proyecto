4d61c0a4613372c211f76be9496d24868d321e6d
Edwin Andres Tapia Lasso <e.tapia@uniandes.edu.co>
2023-10-14 17:45:25 -0500

    Ajuste stilos Página  principal ABC JOBS

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,15 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterTypeComponent } from './register-type/register-type.component';
 import { RegisterComponent } from './register/register.component';
+import { LandingComponent } from './landing/landing.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: '', redirectTo: 'landing', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'register-type', component: RegisterTypeComponent },
   { path: 'register', component: RegisterComponent },
-  { path: '**', redirectTo: '/login' },
-];
+  { path: 'landing', component: LandingComponent },
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
