52e065622de376d89768f86ff4a6dc23acccaca4
Edwin Andres Tapia Lasso <e.tapia@uniandes.edu.co>
2023-10-17 18:46:43 -0500

    merge login3

---
+++ src/app/services/login.service.spec.ts
@@ -0,0 +1,29 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async, inject } from '@angular/core/testing';
+import { LoginService } from './login.service';
+import { HttpClient } from '@angular/common/http';
+
+
+class HttpClientMock {
+  get = jasmine.createSpy('httpClient.get');
+}
+
+describe('Service: Login', () => {
+
+  let service: LoginService;
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [LoginService,
+        {
+          provide: HttpClient,
+          useClass: HttpClientMock
+        }
+      ]
+    });
+  });
+
+  it('should ...', inject([LoginService], (service: LoginService) => {
+    expect(service).toBeTruthy();
+  }));
+});
